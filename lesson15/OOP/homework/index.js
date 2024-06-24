3d41c55aaa43d884634e0a979007bf611614f794
AndreyHein <soly.a@web.de>
2024-06-24 18:27:01 +0200

    Update index.js

--- lesson15/OOP/homework/index.js
+++ lesson15/OOP/homework/index.js
@@ -37,3 +37,26 @@ const arrayOfObjects = (obj) => {
     }));
 }
 console.log(arrayOfObjects(object1));
+
+// Task #5
+object2 = {key1: 'value1', key2: 'value2', key3: 'value3', key4: 'value4', key5: 'value5'};
+object3 = {key3: 'new_value3', key4: 'new_value4', key5: 'new_value5', key6: 'value6', key7: 'value7'};
+
+const mergeObjects = (obj1, obj2) => {
+    const mergeObj = {};
+    for(let key in obj1) {
+        if(key in obj2) {
+            mergeObj[key] = obj2[key];
+        } else {
+            mergeObj[key] = obj1[key];
+        }
+    }
+    return mergeObj;
+}
+console.log(mergeObjects(object2, object3));
+
+console.log("======Alternative======");
+const mergeObjectsAlternative = (obj1, obj2) => {
+    return { ...obj1, ...obj2 };
+}
+console.log(mergeObjectsAlternative(object2, object3));
